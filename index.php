7065e87aefe4b2fa76d33d8204f172e2725fb504
unknown <mcbrideryan08@gmail.com>
2020-05-16 11:59:47 -0400

    Done with About Me Page -Ryan

--- index.php
+++ index.php
@@ -18,12 +18,12 @@
 </head>
 <body>
 
-<div class="jumbotron text-center" style="margin-bottom:0">
-  <h1>My First Bootstrap 4 Page</h1>
-  <p><strong>Step 1: Change the background color of this jumbotron</strong></p>
+<div class="jumbotron text-center" style="margin-bottom:0; background-color:rgb(39, 150, 214); font-family:verdana; color: white;">
+  <h1>Ryan McBride</h1>
+  <p><strong></strong></p>
 </div>
 
-<nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+<nav class="navbar navbar-expand-sm bg-light navbar-light">
   <a class="navbar-brand" href="#">Navbar</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
     <span class="navbar-toggler-icon"></span>
@@ -31,14 +31,14 @@
   <div class="collapse navbar-collapse" id="collapsibleNavbar">
     <ul class="navbar-nav">
       <li class="nav-item">
-        <a class="nav-link" href="#">Link</a>
+        <a class="nav-link" href="https://www.instagram.com/ryanl.m/">Instagram</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="#">Link</a>
+        <a class="nav-link" href="https://www.linkedin.com/in/ryan-mcbride-141b6b18a/">Linkedin</a>
       </li>
-      <li class="nav-item">
+    <!--  <li class="nav-item">
         <a class="nav-link" href="#">Link</a>
-      </li>
+      </li>-->
     </ul>
   </div>
 </nav>
@@ -48,43 +48,39 @@
     <div class="col-sm-4">
       <h2>About Me</h2>
       <h5>Photo of me:</h5>
-      <div class="fakeimg">Replace with a personal photo</div>
-      <p>Some text about me in culpa qui officia deserunt mollit anim..</p>
-      <h3>My Favorite Sites</h3>
-      <p>Here are a few of my favorite sites.</p>
+      <div class="thumbnail"><img src="images/swim.jpg" style="width:80%;"></div>
+      <p>My name is Ryan McBride and I am a junior in high school. I love being involved in school and I love to program! This picture is me after a race!</p>
+      <h3>My Favorite Links</h3>
+      <p>Here are a few of my favorite links!</p>
       <ul class="nav nav-pills flex-column">
         <li class="nav-item">
-          <a class="nav-link active" href="#">Active</a>
+          <a class="nav-link active" href="https://open.spotify.com/user/.swimmaa.?si=xoi_A9b_Q3mvj7e5Vxk8tg">My Spotify</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
+          <a class="nav-link" href="https://www.roblox.com/">Roblox</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link disabled" href="#">Disabled</a>
+          <a class="nav-link" href="https://www.awwwards.com/">Bootstrap References! (awwwards.com)</a>
         </li>
       </ul>
       <hr class="d-sm-none">
     </div>
     <div class="col-sm-8">
       <h2>Most Favorite Class</h2>
-
-      <div class="fakeimg">Replace with your favorite class picture</div>
-      <p>Some text..</p>
-      <p>Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
+      <div class="thumbnail"><img src="images/science.jfif" style="width:50%;"></div>
+      <p>AP Environmental Science</p>
+      <p>AP Environmental is probably one of my favorite classes I've ever taken. I love the teacher and he is well versed in the topics he teaches and he makes everything easier to understand. Because of this class I learned to start caring for the environment more.</p>
       <br>
       <h2>Least Favorite Class</h2>
-      <div class="fakeimg">Replace with your least favorite class picture</div>
-      <p>Some text..</p>
-      <p>Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
+      <div class="thumbnail"><img src="images/pltw.jpg" style="width:50%;"></div>
+      <p>PROJECT LEAD THE WAY [PLTW]</p>
+      <p>My least favorite class is my engineering program "Project Lead the Way" also known as PLTW. This class is stupidly hard and it is the main source of all of my stress in school. It makes me cry and I hate it very much. This picture I took in the class is nice though.</p>
     </div>
   </div>
 </div>
 
-<div class="jumbotron text-center" style="margin-bottom:0">
-  <p><strong>Change the background color of this footer<//p>
+<div class="jumbotron text-center" style="margin-bottom:0; background-color:rgb(39, 150, 214);">
+
 </div>
 
 </body>
